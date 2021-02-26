8c9c684e0f2c2afcea92728f0205e30796b6f3d4
Random-Web-Hosting <salmanhilabi007@gmail.com>
2021-02-26 16:15:28 +0530

    responsive react card completed

--- src/component/cards/card.js
+++ src/component/cards/card.js
@@ -1,8 +1,17 @@
 import React from "react";
+import '../../assets/css/cards.css'
+import noImg from '../../assets/images/noImg.jpg';
 
 const Card = (props) => {
     return <div className="card text-left">
-     <h1>Cards</h1>
+        <div className="overflow">
+            {props.cardImg ?  <img src={props.cardImg} alt="card-img-one" className="card-img-top"/> : <img src={noImg} alt="card-img-one" className="card-img-top"/>}
+        </div>
+        <div className="card-body text-dark">
+            <h3 className="card-title">{props.cardTitle}</h3>
+            <p className="card-text text-secondary">{props.cardText}</p>
+            <a href="https://google.com" target="_blank"  className="card-btn">Click Here</a>
+        </div>
     </div>
 }
 
--- src/component/cards/cardsWrapper.js
+++ src/component/cards/cardsWrapper.js
@@ -1,26 +1,37 @@
 import React from "react";
 import Card from './card';
+import cardImg1 from '../../assets/images/cardimg1.jpg';
+import cardImg2 from '../../assets/images/cardimg2.jpg';
+import cardImg4 from '../../assets/images/cardimg4.jpg';
+import cardImg5 from '../../assets/images/cardimg5.jpg';
+import cardImg6 from '../../assets/images/cardimg6.jpg';
 
 const CardsWrapper = () => {
     return <div className="container d-flex justify-content-center">
         <div className="row">
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardImg={cardImg1} cardTitle="Card One" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardTitle="Card Two" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardImg={cardImg2} cardTitle="Card Two" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardImg={cardImg4} cardTitle="Card Three" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardImg={cardImg5} cardTitle="Card Four" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-12">
-               <Card/>
+                <Card cardImg={cardImg6} cardTitle="Card Five" cardText="react app card text demo, 
+            react app card text demo, react app card text demo"/>
             </div>
         </div>
     </div>
